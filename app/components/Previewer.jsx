dbe866fb49adb181f0793ee639f489ded55424b0
Sanskar Modi <sanskarmodi97@gmail.com>
2016-07-13 00:06:38 +0530

    Added markdown support

--- app/components/Previewer.jsx
+++ app/components/Previewer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import marked from 'marked'
 
 export default class Previewer extends React.Component {
   constructor() {
     super();
     this.handleTextChange = this.handleTextChange.bind(this);
+    this.markup = this.markup.bind(this);
     this.state = {text: ''}
   }
 
@@ -12,13 +14,19 @@ export default class Previewer extends React.Component {
     this.setState({text: e.target.value})
   }
 
+  markup() {
+    return { __html: marked(this.state.text)}
+  }
+
   render() {
     return (
       <div className="previewer">
-        <textarea className="editor" onChange={this.handleTextChange} />
-        <div className="preview">
-          {this.state.text}
-        </div>
+        <textarea
+          className="editor"
+          onChange={this.handleTextChange} />
+        <div
+          className="preview"
+          dangerouslySetInnerHTML={this.markup()} />
       </div>
     )
   }
