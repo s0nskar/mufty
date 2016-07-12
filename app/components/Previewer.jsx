8e7389cdacc6fc5184522be45181b434299ced27
Sanskar Modi <sanskarmodi97@gmail.com>
2016-07-12 23:53:05 +0530

    :neutral_face: Add on handleTextChange method

--- app/components/Previewer.jsx
+++ app/components/Previewer.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 export default class Previewer extends React.Component {
+  constructor() {
+    super();
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.state = {text: ''}
+  }
+
+  handleTextChange(e) {
+    this.setState({text: e.target.value})
+  }
+
   render() {
-    return <h1>Hello World! Hello World! Hello World!</h1>
+    return (
+      <div className="previewer">
+        <textarea className="editor" onChange={this.handleTextChange} />
+        <div className="preview">
+          {this.state.text}
+        </div>
+      </div>
+    )
   }
 }
